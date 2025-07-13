bfffcc0267cf6082c16817c2c9ec38e4a8d0204e
zsh28 <zeeshan8210@outlook.com>
2025-07-13 23:51:11 +0100

    Implement solutions and tests for iterators and error handling exercises

--- exercises/18_iterators/iterators2.rs
+++ exercises/18_iterators/iterators2.rs
@@ -7,7 +7,11 @@ fn capitalize_first(input: &str) -> String {
     let mut chars = input.chars();
     match chars.next() {
         None => String::new(),
-        Some(first) => todo!(),
+        Some(first) => {
+            let mut capitalized = first.to_uppercase().to_string();
+            capitalized.extend(chars);
+            capitalized
+        }
     }
 }
 
@@ -15,14 +19,14 @@ fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    // ???
+    words.iter().map(|&word| capitalize_first(word)).collect()
 }
 
 // TODO: Apply the `capitalize_first` function again to a slice of string
 // slices. Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 fn capitalize_words_string(words: &[&str]) -> String {
-    // ???
+    words.iter().map(|&word| capitalize_first(word)).collect::<Vec<String>>().join("")
 }
 
 fn main() {
